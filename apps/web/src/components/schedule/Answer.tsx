2556b30e47b5c7be64719fcf9e4de79e432583f7
aokuyama <ao980s@gmail.com>
2023-05-02 10:53:58 +0000

    add answer form

--- apps/web/src/components/schedule/Answer.tsx
+++ apps/web/src/components/schedule/Answer.tsx
@@ -2,12 +2,16 @@ import { FC, useState } from "react";
 import { UpcomingEvent } from "domain/src/model/event";
 import { YesOrNoList } from "ui/src/components/YesOrNoList";
 import { AttendanceList } from "domain/src/model/event/attendance";
+import { TextBox } from "ui/src/components/TextBox";
+import { Button } from "ui/src/components/Button";
+import { client } from "infra/src/client/trpc";
 
 interface Props {
   event: UpcomingEvent;
 }
 
 export const Answer: FC<Props> = ({ event }) => {
+  const [name, setName] = useState<string>("");
   const [attendance, setAttendance] = useState<AttendanceList>(
     event.newAttendance()
   );
@@ -21,5 +25,26 @@ export const Answer: FC<Props> = ({ event }) => {
     );
   };
 
-  return <YesOrNoList items={attendance.list()} onChange={onChange} />;
+  const publish = async () => {
+    const result = await client.event.respondAttendance.mutate({
+      event: event.path,
+      name: name,
+      attendance: attendance.value,
+    });
+    console.warn(result.guestId);
+  };
+
+  return (
+    <>
+      <TextBox value={name} setValue={setName} />
+      <YesOrNoList items={attendance.list()} onChange={onChange} />
+      <Button
+        onClick={() => {
+          publish();
+        }}
+      >
+        決定
+      </Button>
+    </>
+  );
 };
