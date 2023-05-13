b95a1de6c75767ba513b93f825b911784e96a8fc
aokuyama <ao980s@gmail.com>
2023-05-13 09:15:08 +0900

    use hooks

---
+++ apps/web/src/hooks/useEvent.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { client } from "infra/src/client/trpc";
+import { UpcomingEvent } from "domain/src/model/event";
+import { storage } from "@/registry";
+import { useRouter } from "next/router";
+
+export const useEvent = (): {
+  event: UpcomingEvent | null | undefined;
+  setEvent: React.Dispatch<
+    React.SetStateAction<UpcomingEvent | null | undefined>
+  >;
+} => {
+  const [event, setEvent] = useState<UpcomingEvent | null | undefined>(
+    undefined
+  );
+
+  const eq = useEventQuery();
+  useEffect(() => {
+    const load = async () => {
+      if (eq) {
+        const ev = await loadEvent(eq);
+        setEvent(ev);
+      }
+    };
+    load();
+  }, [eq]);
+
+  return { event, setEvent };
+};
+
+const loadEvent = async (
+  path: string
+): Promise<UpcomingEvent | null | undefined> => {
+  if (path.length == 0) {
+    return null;
+  }
+  const result = await client.event.getEventByPath.query({
+    token: storage.getUser().getRawToken(),
+    path: path,
+  });
+  if (result.event === undefined) {
+    return undefined;
+  }
+  if (!result.event) {
+    return null;
+  }
+
+  return UpcomingEvent.new(result.event);
+};
+
+const useEventQuery = (): string | undefined => {
+  const [eventQuery, setEventQuery] = useState<string | undefined>(undefined);
+  const router = useRouter();
+  const query = router.query;
+
+  useEffect(() => {
+    if (router.isReady) {
+      const e = query.e;
+      setEventQuery(Array.isArray(e) ? e[0] : e ? e : "");
+    }
+  }, [query, router]);
+  return eventQuery;
+};
--- apps/web/src/pages/e.tsx
+++ apps/web/src/pages/e.tsx
@@ -1,26 +1,15 @@
 import { Head } from "@/components/Head";
 import { Layout } from "@/components/Layout";
-import { FC, useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { client } from "infra/src/client/trpc";
+import { FC, useState } from "react";
 import { UpcomingEvent } from "domain/src/model/event";
 import { Page } from "@/components/pages/view_event/Page";
 import { EventGuest } from "domain/src/model/guest";
-import { Site, storage } from "@/registry";
+import { Site } from "@/registry";
+import { useEvent } from "@/hooks/useEvent";
 
 export const Schedule: FC = () => {
-  const router = useRouter();
-  const { e } = router.query;
-  const [event, setEvent] = useState<UpcomingEvent | null | undefined>();
+  const { event, setEvent } = useEvent();
   const [targetGuest, setTargetGuest] = useState<EventGuest | null>(null);
-
-  useEffect(() => {
-    const load = async () => {
-      const ev = await loadEvent(Array.isArray(e) ? e[0] : e ? e : "");
-      setEvent(ev);
-    };
-    load();
-  }, [e]);
 
   return (
     <>
@@ -41,28 +30,9 @@ export const Schedule: FC = () => {
 
 export default Schedule;
 
-const loadEvent = async (
-  path: string
-): Promise<UpcomingEvent | null | undefined> => {
-  if (path.length == 0) {
-    return null;
-  }
-  const result = await client.event.getEventByPath.query({
-    token: storage.getUser().getRawToken(),
-    path: path,
-  });
-  if (result.event === undefined) {
-    return undefined;
-  }
-  if (!result.event) {
-    return null;
-  }
-  return UpcomingEvent.new(result.event);
-};
-
 const title = (event: UpcomingEvent | null | undefined): string => {
   if (event) {
-    return event.name + " by " + Site.name;
+    return event.name + " はいつがいいですか？ by " + Site.name;
   }
   return Site.name;
 };
