cac57cb41a275013e41995a0ac87976e09adc8ca
aokuyama <ao980s@gmail.com>
2023-05-14 17:37:37 +0900

    fixed server deploy

--- apps/server/src/lambda.ts
+++ apps/server/src/lambda.ts
@@ -2,6 +2,9 @@ import { awsLambdaRequestHandler } from "@trpc/server/adapters/aws-lambda";
 import { appRouter } from "infra/src/trpc";
 import { container } from "./registry";
 
+// DI
+container;
+
 export const handler = awsLambdaRequestHandler({
   router: appRouter,
 });
