36f59cb27eb409602af5084568d77b0508ba5321
aokuyama <ao980s@gmail.com>
2023-05-14 11:01:14 +0900

    fixed deploy setting

--- packages/infra/src/client/trpc/index.ts
+++ packages/infra/src/client/trpc/index.ts
@@ -1,14 +1,10 @@
 import type { AppRouter } from "../../trpc";
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 
-if (!process.env.NEXT_PUBLIC_URL_API) {
-  throw new Error("undefined NEXT_PUBLIC_URL_API");
-}
-
 export const client = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: process.env.NEXT_PUBLIC_URL_API,
+      url: process.env.NEXT_PUBLIC_URL_API || "/api",
     }),
   ],
 });
