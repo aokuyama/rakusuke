b710a46bb65dff137a4c497b17b8ee2192ddeda8
aokuyama <ao980s@gmail.com>
2023-06-14 10:15:57 +0900

    add external link

--- apps/web/src/components/Layout.tsx
+++ apps/web/src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Header } from "ui/src/components/layout/Header";
 import { Site } from "infra/src/web/site";
-import { Footer } from "ui/src/components/layout/Footer";
+import { Footer, LinkProps } from "ui/src/components/layout/Footer";
 import { Dev } from "@/features/dev/components/Dev";
 
 type Props = {
@@ -9,11 +9,22 @@ type Props = {
 };
 
 export const Layout: FC<Props> = ({ children }) => {
+  const link: LinkProps[] = [
+    {
+      title: "GitHub",
+      url: "https://github.com/aokuyama/rakusuke",
+      icon: "github",
+    },
+    {
+      title: "Release notes",
+      url: "https://aokuyama.notion.site/a5d36c765387437db4ab7dc24eb391af",
+    },
+  ];
   return (
     <>
       <Header title={Site.name} slogan={Site.slogan} />
       {children}
-      <Footer title={Site.name} />
+      <Footer title={Site.name} link={link} />
       <Dev />
     </>
   );
--- packages/ui/src/components/layout/Footer.tsx
+++ packages/ui/src/components/layout/Footer.tsx
@@ -1,33 +1,74 @@
 import { FC } from "react";
 import { css } from "@emotion/react";
 import { black, white } from "../../styles/color";
+import { font } from "../../styles/size";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
+export type LinkProps = {
+  title: string;
+  url: string;
+  icon?: "github";
+};
 
 type Props = {
   title?: string;
+  link: LinkProps[];
 };
 
-export const Footer: FC<Props> = ({ title }) => {
+export const Footer: FC<Props> = ({ title, link }) => {
   return (
     <footer css={style}>
-      <p>@ 2023 {title} All rights reserved.</p>
+      <ul>
+        {link.map((link, index) => {
+          return (
+            <li key={index}>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">
+                {link.icon === "github" ? (
+                  <FontAwesomeIcon icon={faGithub} width={20} />
+                ) : (
+                  <FontAwesomeIcon icon={faCircle} width={4} />
+                )}
+                <span>{link.title}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p>© 2023 {title}</p>
     </footer>
   );
 };
 
-export const footerHeight = "52px";
+export const footerHeight = "99px";
 
 const style = css`
   height: ${footerHeight};
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  padding: 20px 16px 16px;
+  padding: 24px 0 16px;
   color: ${white.default};
   background-color: ${black.default};
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   p {
-    margin: 0;
-    font-weight: 200;
-    font-size: 0.8em;
+    text-align: center;
+    margin: 0 auto;
+    font-size: ${font.small}px;
+    color: ${white.disabled};
+  }
+  ul {
+    display: flex;
+    margin: 0 auto;
+    li {
+      margin: 0 8px;
+      a {
+        display: flex;
+        align-items: center;
+        svg {
+          margin: 0 4px;
+        }
+      }
+    }
   }
 `;
